6ba047b9dc2faf85b1c1496bdec01c22d660fe38
Nedeljko Babic <bnedeljko0@gmail.com>
2023-06-28 18:27:30 +0200

    added the extended information about order

--- WebShopREST/WebContent/app/kupacSvaIznajmljivanja.js
+++ WebShopREST/WebContent/app/kupacSvaIznajmljivanja.js
@@ -19,23 +19,27 @@ Vue.component("iznajmlivanja-kupac", {
           krajnjiDatum: null
         }
       },
-      porudzbine: []
+      porudzbine: [],
+      aktivanIndex: -1
     };
   },
-  template: `
-    <div>
+   template: `
+   <div>
       <kupac-menu :korisnik="korisnik"></kupac-menu>
       <div class="porudzbine-container">
         <h2>Prikaz vaših porudžbina:</h2>
         <div v-for="porudzbina in porudzbine" :key="porudzbina.idNarudzbe" class="porudzbina-card">
           <h3 class="narudzba-id">Narudžba ID: {{ porudzbina.idNarudzbe }}</h3>
-          <div v-for="vozilo in porudzbina.iznajmljenaVozila" :key="vozilo.id" class="vozilo-card">
+          <div v-for="(vozilo, index) in porudzbina.iznajmljenaVozila" :key="vozilo.id" class="vozilo-card" @click="prikaziDatume(index)">
             <div class="vozilo-image">
               <img :src="vozilo.slika" alt="Vozilo slika" />
             </div>
             <div class="vozilo-info">
               <p class="marka-model">Marka: {{ vozilo.marka }}</p>
               <p class="marka-model">Model: {{ vozilo.model }}</p>
+              <p v-if="aktivanIndex === index">Datum iznajmljivanja: {{ porudzbina.datumIznajmljivanja }}</p>
+              <p v-if="aktivanIndex === index">Datum vraćanja: {{ porudzbina.datumVracanja }}</p>
+              <button v-if="porudzbina.status === 'Vraceno' && aktivanIndex === index" class="buttonOceniObjekat" style="font-size: 13px; padding: 12px 24px;">Ocijeni objekat</button>
             </div>
           </div>
           <p class="cena-narudzbe">Cena narudžbe: {{ porudzbina.cena }}</p>
@@ -60,35 +64,42 @@ Vue.component("iznajmlivanja-kupac", {
       });
   },
   methods: {
+	  prikaziDatume(index) {
+      if (this.aktivanIndex === index) {
+        this.aktivanIndex = -1; 
+      } else {
+        this.aktivanIndex = index;
+      }
+    },
     otkaziNarudzbinu(porudzbina) {
       if (porudzbina.status === 'Obrada') {
         porudzbina.status = 'Otkazano';
-      
-       axios.put('rest/porudzbine/izmeniporudzbinu', porudzbina)
-        .then(response => {
-          console.log(response.data);
-          if (response.data === true) {
-            console.log("Upsješno otkazana porudzbina.");           
+        
+        axios.put('rest/porudzbine/izmeniporudzbinu', porudzbina)
+          .then(response => {
+            console.log(response.data);
+            if (response.data === true) {
+              console.log("Upsješno otkazana porudzbina.");           
             
-            const ukupnaCena = porudzbina.cena;
-            const brojBodova = -(ukupnaCena / 1000 * 133 * 4);
-            axios.put(`rest/korisnici/azurirajBodove/` + brojBodova)
-              .then(response => {
-                this.korisnik.brojBodova = response.data.brojBodova;
-              })
-              .catch(error => {
-                console.error(error);
-              });
-            const index = this.porudzbine.indexOf(porudzbina);
-            if (index !== -1) {
-              this.porudzbine.splice(index, 1);
+              const ukupnaCena = porudzbina.cena;
+              const brojBodova = -(ukupnaCena / 1000 * 133 * 4);
+              axios.put(`rest/korisnici/azurirajBodove/` + brojBodova)
+                .then(response => {
+                  this.korisnik.brojBodova = response.data.brojBodova;
+                })
+                .catch(error => {
+                  console.error(error);
+                });
+              const index = this.porudzbine.indexOf(porudzbina);
+              if (index !== -1) {
+                this.porudzbine.splice(index, 1);
+              }
             }
-          }
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }
-    }
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+    },
   }
 });
